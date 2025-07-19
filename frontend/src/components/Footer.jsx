a8435ec8982871a75a0a243125ed639819b4ac22
sahilchalke0001 <sahilchalke0001@gmail.com>
2025-07-19 22:24:40 +0530

    update1

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="barca-footer">
       <div className="footer-content">
-        <p>© 2025 Coach Pedro Gonçalves. All rights reserved.</p>
+        <p>© 2025 Coach Pedro Gonçalves. All rights reserved</p>
         <p>🔵🔴Visca el Barça! 🔴🔵</p>
       </div>
     </footer>
